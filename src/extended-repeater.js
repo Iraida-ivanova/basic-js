41a1e6fe50a44b4324fcefb7e49e04847021c860
Iraida22 <iraida.iva22@mail.ru>
2022-05-02 00:58:31 +0300

    feat: add completed extended-repeater, matrix-elements-sum, sum-digits

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options = {}) {
+  let resultString = "";
+  if (typeof str !== "string") {
+      str = String(str);
+  }
+  if (!options.hasOwnProperty("repeatTimes")) {
+      options.repeatTimes = 1;
+  }
+  if (!options.hasOwnProperty("separator")) {
+      options.separator = "+";
+  }
+  if (!options.hasOwnProperty("additionSeparator")) {
+      options.additionSeparator = "|";
+  }
+  if (!options.hasOwnProperty("addition")) {
+      while (options.repeatTimes > 1) {
+          resultString += str + options.separator;
+          options.repeatTimes--;
+      }
+      resultString += str;
+  } else if (!options.hasOwnProperty("additionRepeatTimes")) {
+      while (options.repeatTimes > 1) {
+          resultString += str + options.addition + options.separator;
+          options.repeatTimes--;
+      }
+      resultString += str + options.addition;
+  } else {
+      let additionString = "";
+      while (options.additionRepeatTimes > 1) {
+          additionString += options.addition + options.additionSeparator;
+          options.additionRepeatTimes--;
+      }
+      additionString += options.addition;
+      while (options.repeatTimes > 1) {
+          resultString += str + additionString + options.separator;
+          options.repeatTimes--;
+      }
+      resultString += str + additionString;
+  }
+  return resultString;
 }
 
 module.exports = {
